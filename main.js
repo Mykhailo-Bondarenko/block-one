ec8c6638f32c32bebb378db9695af5a5c3397835
Mykhailo-Bondarenko <mikhailzyagun@gmail.com>
2022-02-14 16:16:39 +0200

    fixed several bugs.

--- main.js
+++ main.js
@@ -353,14 +353,13 @@ const memoFactorial = (function () {
     if (arguments.length > 1) {
       throw new Error('There must be only one argument.');
     }
-    let result;
     if (num in memo) {
       return memo[num];
     }
     if (num === 0 || num === 1) {
       return 1;
     }
-    result = factorial(num - 1);
+    let result = factorial(num - 1);
     memo[num] = result;
     return num * memo[num];
   };
@@ -594,10 +593,10 @@ function averageNumberMatrix(array, callback) {
   if (arguments.length > 2) {
     throw new Error('There must be only two argument.');
   }
+  let result = 0;
   if (array.length < 1) {
     return result;
   }
-  let result = 0;
   let counter = 0;
   for (let i = 0; i < array.length; i++) {
     if (!Array.isArray(array[i])) {
@@ -688,9 +687,12 @@ function deleteRowMatrix(array, callback) {
     if (!Array.isArray(array[i])) {
       throw new Error('Must be a two-dimensional arrays.');
     }
-    if (array[i].some(callback)) {
-      array.splice(i, 1);
-      i--;
+    for (let j = 0; j < array[i].length; j++) {
+      if (callback(array[i][j])) {
+        array.splice(i, 1);
+        i--;
+        break;
+      }
     }
   }
   return array;
@@ -700,11 +702,15 @@ function deleteColumnMatrix(array, callback) {
   if (!Array.isArray(array) || typeof callback !== 'function') {
     throw new Error('Array must be array, callback must be a function.');
   }
+  let result = [];
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array[i].length; j++) {
       if (callback(array[i][j])) {
         for (let k = 0; k < array.length; k++) {
           array[k].splice(j, 1);
+          if (array[k].length < 1) {
+            return result;
+          }
         }
       }
     }
@@ -716,11 +722,11 @@ function deleteColumnMatrix(array, callback) {
 
 // Посчитать сумму / количество нулевых элементов / среднее значение элементов матрицы над и под главной диагональю и на главной диагонали.
 
-function findSumDiagonal(array) {
+function findElementsDiagonal(array) {
   if (!Array.isArray(array)) {
     throw new Error('Array must be array.');
   }
-  let sum = 0;
+  const result = [];
   for (let i = 0; i < array.length; i++) {
     if (array[i].length !== array.length) {
       throw new Error('Array must be a square matrix.');
@@ -728,16 +734,16 @@ function findSumDiagonal(array) {
     if (typeof array[i][i] !== 'number') {
       throw new Error('Elements must be only a number.');
     }
-    sum += array[i][i];
+    result.push(array[i][i]);
   }
-  return sum;
+  return result;
 }
 
-function findSumAboveDiagonal(array) {
+function findElementsAboveDiagonal(array) {
   if (!Array.isArray(array)) {
-    throw new Error('Array must be a array.');
+    throw new Error('Array must be array.');
   }
-  let sum = 0;
+  const result = [];
   for (let i = 0; i < array.length; i++) {
     if (array[i].length !== array.length) {
       throw new Error('Array must be a square matrix.');
@@ -746,17 +752,17 @@ function findSumAboveDiagonal(array) {
       if (typeof array[i][j] !== 'number') {
         throw new Error('Elements must be only a number.');
       }
-      sum += array[i][j];
+      result.push(array[i][j]);
     }
   }
-  return sum;
+  return result;
 }
 
-function findSumUnderDiagonal(array) {
+function findElementsUnderDiagonal(array) {
   if (!Array.isArray(array)) {
     throw new Error('Array must be array.');
   }
-  let sum = 0;
+  const result = [];
   for (let i = 1; i < array.length; i++) {
     if (array[i].length !== array.length) {
       throw new Error('Array must be a square matrix.');
@@ -765,130 +771,56 @@ function findSumUnderDiagonal(array) {
       if (typeof array[i][j] !== 'number') {
         throw new Error('Elements must be only a number.');
       }
-      sum += array[i][j];
+      result.push(array[i][j]);
     }
+  }
+  return result;
+}
+
+function findSumElements(array) {
+  if (!Array.isArray(array)) {
+    throw new Error('Array must be array.');
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+
+    if (typeof array[i] !== 'number') {
+      throw new Error('Elements must be only a number.');
+    }
+    sum += array[i];
   }
   return sum;
 }
 
-function findAmountDiagonal(array, callback) {
+function findAmountElements(array, callback) {
   if (!Array.isArray(array) || typeof callback !== 'function') {
     throw new Error('Array must be array and callback a function.');
   }
   let amount = 0;
   for (let i = 0; i < array.length; i++) {
-    if (array[i].length !== array.length) {
-      throw new Error('Array must be a square matrix.');
-    }
-    if (typeof array[i][i] !== 'number') {
+
+    if (typeof array[i] !== 'number') {
       throw new Error('Elements must be only a number.');
     }
-    if (callback(array[i][i])) {
+    if (callback(array[i])) {
       amount++;
     }
   }
   return amount;
 }
 
-function findAmounAboveDiagonal(array, callback) {
-  if (!Array.isArray(array) || typeof callback !== 'function') {
-    throw new Error('Array must be array and callback a function.');
-  }
-  let amount = 0;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i].length !== array.length) {
-      throw new Error('Array must be a square matrix.');
-    }
-    for (let j = i + 1; j < array[i].length; j++) {
-      if (typeof array[i][j] !== 'number') {
-        throw new Error('Elements must be only a number.');
-      }
-      if (callback(array[i][j])) {
-        amount++;
-      }
-    }
-  }
-  return amount;
-}
-
-function findAmountUnderDiagonal(array, callback) {
-  if (!Array.isArray(array) || typeof callback !== 'function') {
-    throw new Error('Array must be array and callback a function.');
-  }
-  let amount = 0;
-  for (let i = 1; i < array.length; i++) {
-    if (array[i].length !== array.length) {
-      throw new Error('Array must be a square matrix.');
-    }
-    for (let j = 0; j < i; j++) {
-      if (typeof array[i][j] !== 'number') {
-        throw new Error('Elements must be only a number.');
-      }
-      if (callback(array[i][j])) {
-        amount++;
-      }
-    }
-  }
-  return amount;
-}
-
-function findAverageDiagonal(array) {
+function findAverageElements(array) {
   if (!Array.isArray(array)) {
     throw new Error('Array must be array.');
   }
   let sum = 0;
   let counter = 0;
   for (let i = 0; i < array.length; i++) {
-    if (array[i].length !== array.length) {
-      throw new Error('Array must be a square matrix.');
-    }
-    if (typeof array[i][i] !== 'number') {
+    if (typeof array[i] !== 'number') {
       throw new Error('Elements must be only a number.');
     }
-    sum += array[i][i];
+    sum += array[i];
     counter++;
-  }
-  return sum / counter;
-}
-
-function findAverageAboveDiagonal(array) {
-  if (!Array.isArray(array)) {
-    throw new Error('Array must be array.');
-  }
-  let sum = 0;
-  let counter = 0;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i].length !== array.length) {
-      throw new Error('Array must be a square matrix.');
-    }
-    for (let j = i + 1; j < array[i].length; j++) {
-      if (typeof array[i][j] !== 'number') {
-        throw new Error('Elements must be only a number.');
-      }
-      sum += array[i][j];
-      counter++;
-    }
-  }
-  return sum / counter;
-}
-
-function findAverageUnderDiagonal(array) {
-  if (!Array.isArray(array)) {
-    throw new Error('Array must be array.');
-  }
-  let sum = 0;
-  let counter = 0;
-  for (let i = 1; i < array.length; i++) {
-    if (array[i].length !== array.length) {
-      throw new Error('Array must be a square matrix.');
-    }
-    for (let j = 0; j < i; j++) {
-      if (typeof array[i][j] !== 'number') {
-        throw new Error('Elements must be only a number.');
-      }
-      sum += array[i][j];
-      counter++;
-    }
   }
   return sum / counter;
 }
@@ -1010,9 +942,10 @@ function calculateСountBits(number) {
   result.one = 0;
   result.zero = 0;
   for (let i = 0; i < 32; i++) {
-    (number & (1 << i)) === (1 << i)
-      ? result.one++
-      : result.zero++;
+    if ((number & (1 << i)) === (1 << i)) {
+      result.one++;
+    }
+    result.zero = 32 - result.one;
   }
   return result;
 }
